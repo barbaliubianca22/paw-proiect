bbb956e7afad48399319c4b5b7c11fd6c57a530e
Diana-Craiu <diana-mihaela.craiu@student.tuiasi.ro>
2023-12-24 17:22:36 +0200

    Actualizare SVG-uri

--- Front/CodeWiz/src/app/app.module.ts
+++ Front/CodeWiz/src/app/app.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { CircleSvgComponent } from './acasa/circle-svg.component';
 import { AppComponent } from './app.component';
 import { AcasaComponent } from './acasa/acasa.component'; // Asigură-te că ai importat AcasaComponent
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AcasaComponent, // Asigură-te că AcasaComponent este inclus aici
-    // ... alte componente
+    AcasaComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- Front/CodeWiz/src/app/app.routes.ts
+++ Front/CodeWiz/src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { AcasaComponent } from './acasa/acasa.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'acasa', pathMatch: 'full' },
     { path: 'acasa', component: AcasaComponent },
+    { path: 'login', component: LoginComponent },
     { path: '**',component: AcasaComponent },
 ];
